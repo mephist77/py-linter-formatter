d7dad17d329f74ec6882e974b5e65c1fec78623b
Meph <wpavlow@gmail.com>
2026-03-05 10:46:22 -0300

    lala

--- app/main.py
+++ app/main.py
@@ -7,8 +7,9 @@ def format_linter_error(error: dict) -> dict:
         "source" : "flake8"
     } if error else []
 
+
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    return{
+    return {
         "errors": [
             format_linter_error(error) for error in errors
         ],
@@ -16,5 +17,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "status": ("failed" if any(errors) else "passed")
     }
 
+
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path = key, errors = value) for key , value in linter_report.items()]
+    return [format_single_linter_file(file_path= key, errors= value) for key , value in linter_report.items()]
