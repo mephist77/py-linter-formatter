014f0208ec92b8553ac0ff62e3b56f8c4ee5d963
Meph <wpavlow@gmail.com>
2026-03-05 10:54:31 -0300

    ??

--- app/main.py
+++ app/main.py
@@ -19,5 +19,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path= key, errors= value) for key,
-    value in linter_report.items()]
+    return [format_single_linter_file(file_path=key, errors=value)
+            for key, value in linter_report.items()]
