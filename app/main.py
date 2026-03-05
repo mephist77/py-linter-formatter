4cf11ba9fa5df50f0b0183e6cc08c5d28a104744
Meph <wpavlow@gmail.com>
2026-03-05 10:40:45 -0300

    done

--- app/main.py
+++ app/main.py
@@ -20,7 +20,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return {
-        "errors" : format_linter_error(value),
-        "path" : (key),
-        "status" : "failed" if value else "passed"} for key, value in linter_report.items()
+    return [format_single_linter_file(file_path = key, errors = value) for key , value in linter_report.items()]
